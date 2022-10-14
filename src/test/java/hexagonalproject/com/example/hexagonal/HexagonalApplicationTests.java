0faa49cf807ee20d54ad139ac0d9757a9a0b73a5
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-10-14 10:25:57 +0700

    initialize

---
+++ src/test/java/hexagonalproject/com/example/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package hexagonalproject.com.example.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
